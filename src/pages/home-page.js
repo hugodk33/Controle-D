2ff778ccfe104c0feabb6fb1bf388a861d0c48e9
hugodk33 <hugodk33@gmail.com>
2020-01-30 18:05:52 -0300

    tela de login

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
+import Input from './templates/Input';
+
 const Home = () => {
 
   return (
-    <div> testess
+    <div className="container"> 
+      <div className="row"> 
+        <div className="col-4 col-sm-2  col-md-4"> 
+          <br />
+        </div>
+        <div className="col-12 col-sm-8 col-md-4"> 
+          <h4 style={{textAlign: "center"}}>Entrar</h4>
+          <br /><br /><br />
+          <Input  id="Email-user" label="EMAIL" placeholder="Usuario@Email.com" type={"text"} col="12"/>
+          <Input  id="Senha" label="SENHA" placeholder="***********" type={"password"} col="12"/>
+        </div>        
+      </div>
     </div>
   );
 };
