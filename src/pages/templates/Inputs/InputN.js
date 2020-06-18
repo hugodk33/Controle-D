6ddb378fcb9c179667236bee6f04baace9930697
Thiago Neto <thiagonetorj@hotmail.com>
2020-06-18 13:24:19 -0300

    input numeral incrementando e decrementando

--- src/pages/templates/Inputs/InputN.js
+++ src/pages/templates/Inputs/InputN.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {SinaldeMais , SinaldeMenos} from '../icons/icones-botoes'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
@@ -33,18 +33,45 @@ const InputN = (props) => {
     const classes = useStyles();
     const classes2 = useStyles2();
 
+    const [value, setValue] = useState("")
+
+    const getIntValue = () => Number.parseInt((value || 0))
+    const setStrValue = (valor) => {
+        const strValue = valor.toString()
+        setValue(strValue)
+        if(props.onChange)
+            props.onChange(strValue)
+    }
+
+    const increment = () => {
+        let valor = getIntValue()
+        if(valor < 120){
+            valor += 1
+        }
+        setStrValue(valor)
+    }
+
+    const decrement = () => {
+        let valor = getIntValue()
+        if(valor > 0){
+            valor -= 1
+        }
+
+        setStrValue(valor)
+    }
+
     return (
     <div className={"col-" + props.col}>
         <div className="holder-inputN">
             <form className={classes.root} noValidate autoComplete="off">
-                <TextField size="small" id="outlined-basic" label={props.label} variant="outlined" />
+                <TextField size="small" id="outlined-basic" onChange={event => setValue(event.target.value)} label={props.label} value={value} variant="outlined" />
             </form>
         </div>
         <div className="holder-btn-inputN">
-            <Button variant="contained"  className={classes2.root} color="primary">
+            <Button variant="contained"  className={classes2.root} onClick={() => increment()} color="primary">
                 <SinaldeMais />
             </Button>
-            <Button variant="contained"  className={classes2.root} color="primary">
+            <Button variant="contained"  className={classes2.root}  onClick={() => decrement()}  color="primary">
                 <SinaldeMenos />
             </Button>
         </div>
